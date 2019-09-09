3bada00c632af790fcf5316b9dc1578fd0e49159
João Pedro Azevedo (1111476) <1111476@isep.ipp.pt>
2019-09-09 22:45:09 +0100

    server and client socket test updated

--- socket/server.c
+++ socket/server.c
@@ -1,20 +1,59 @@
 
 // Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
 #include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h>
+#include <sys/ioctl.h> 
+#include <netinet/in.h> 
 #include <arpa/inet.h>	//inet_addr
-#include <string.h> 
-#define PORT 8080 
+#include <string.h>
+#include <net/if.h>
+
+#define PORT 8080
+
+void getIp(char* iface, char *result){
+
+    unsigned char ip_address[15];
+    int fd;
+    struct ifreq ifr;
+     
+    /*AF_INET - to define network interface IPv4*/
+    /*Creating soket for it.*/
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+     
+    /*AF_INET - to define IPv4 Address type.*/
+    ifr.ifr_addr.sa_family = AF_INET;
+     
+    /*eth0 - define the ifr_name - port name
+    where network attached.*/
+    memcpy(ifr.ifr_name, iface , IFNAMSIZ-1);
+     
+    /*Accessing network interface information by
+    passing address using ioctl.*/
+    ioctl(fd, SIOCGIFADDR, &ifr);
+    /*closing fd*/
+    close(fd);
+     
+    /*Extract IP Address*/
+    strcpy(ip_address,inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+     
+    //printf("System IP Address is: %s\n",ip_address);
+
+    sprintf(result, "Message from Socket Server on %s\n", ip_address);
+}
+
 
 int main(int argc, char const *argv[]) 
 {
 	int socket_desc , new_socket , c, valread;
 	char buffer[1024] = {0}; 
 	struct sockaddr_in server , client;
-	char *message;
+	char *iface_name = argv[1];
+	char srv_message[100] = "";
 	
 	//Create socket
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -50,8 +89,9 @@ int main(int argc, char const *argv[])
 		//Reply to the client
 		valread = read( new_socket , buffer, 1024); 
 		printf("%s\n",buffer ); 
-		message = "Hello Client , I have received your connection. But I have to go now, bye\n";
-		write(new_socket , message , strlen(message));
+		getIp(iface_name, srv_message);
+		//message = "Hello Client , I have received your connection. But I have to go now, bye\n";
+		write(new_socket , srv_message , strlen(srv_message));
 	}
 	
 	if (new_socket < 0)
