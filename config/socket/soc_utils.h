8546832a478965d982e995fdff6087ef942adad1
João Azevedo <1111476@isep.ipp.pt>
2020-01-16 22:06:48 +0000

    repo modification

---
+++ config/socket/soc_utils.h
@@ -0,0 +1,30 @@
+#ifndef SOC_UTILS_H_
+#define SOC_UTILS_H_
+/* Includes section */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h> 
+#include <sys/ioctl.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <string.h>
+#include <net/if.h>
+
+/* Constant variables / MACROS */
+#define PORT 8080
+/* Function Headers */
+void initClient(char *iface_name, char *srv_ip);
+
+void checkHostName(int hostname);
+void checkHostEntry(struct hostent * hostentry);
+void getHostandIp(char* iface, char *result);
+
+void initServer(char *iface_name);
+void getIp(char* iface, char *result);
+
+
+#endif /* SOC_UTILS_H_*/
--- scripts/general/init_qgc.py
+++
@@ -1,5 +0,0 @@
-import os
-import subprocess
-
-qgc_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", "..", ".."))
-subprocess.call(qgc_path+"/QGroundControl.AppImage", shell=True);
