7b63bd3d608a5bafe97e6eb836ccba5ce7d3652f
João Azevedo <1111476@isep.ipp.pt>
2020-02-05 22:58:53 +0000

    changes on app

--- app/app_tk.py
+++ app/app_tk.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 from tkinter import *
 
 id = 0
@@ -17,28 +19,28 @@ class Application:
         self.container2["pady"] = 5
         self.container2.pack()
 
-        self.lblbase = Label(self.container2, 
-        text="Base:", font=self.fonte, width=15)
-        self.lblbase.pack(side=LEFT)
+        self.lblqgc = Label(self.container2, 
+        text="qGroundControl:", font=self.fonte, width=15)
+        self.lblqgc.pack(side=LEFT)
 
-        self.config_base = Button(self.container2, text="Config", 
+        self.open_qGC = Button(self.container2, text="Open", 
         font=self.fonte, width=15)
-        self.config_base["command"] = self._config_base
-        self.config_base.pack(side=RIGHT)
+        self.open_qGC["command"] = self._open_qgc
+        self.open_qGC.pack(side=RIGHT)
 
         self.container3 = Frame(master)
         self.container3["padx"] = 20
         self.container3["pady"] = 5
         self.container3.pack()
 
-        self.lblqgc = Label(self.container3, 
-        text="qGroundControl:", font=self.fonte, width=15)
-        self.lblqgc.pack(side=LEFT)
+        self.lblbase = Label(self.container3, 
+        text="Base:", font=self.fonte, width=15)
+        self.lblbase.pack(side=LEFT)
 
-        self.open_qGC = Button(self.container3, text="Open", 
+        self.config_base = Button(self.container3, text="Config", 
         font=self.fonte, width=15)
-        self.open_qGC["command"] = self._open_qgc
-        self.open_qGC.pack(side=RIGHT)
+        self.config_base["command"] = self._config_base
+        self.config_base.pack(side=RIGHT)
 
         self.container4 = Frame(master)
         self.container4["padx"] = 20
@@ -59,56 +61,12 @@ class Application:
         self.exit_btn["command"] = quit
         self.exit_btn.pack(side=BOTTOM)
 
-    def command(self):
-        print("button clicked")
-
-    
     def _open_qgc(self):
         print("open_qgc")
+        qgc_path = os.path.abspath(os.path.join(__file__, "..", "..", "..", "..", ".."))
+        subprocess.Popen(qgc_path+"/QGroundControl.AppImage", shell=True)
 
-    def _config_base(self):
-        print("config_base")
-    
-    def _start_drone(self, btn_id):
-        print("start_drone")
-
-    def _config_drone(self, btn_id, master=None):
-        print("config_drone id: " + str(btn_id))
-        
-        self.newwin = Toplevel(master)
-
-        self.newwin.container = Frame(self.newwin)
-        self.newwin.container["pady"] = 10
-        self.newwin.container.pack()
-
-        self.newwin.title = Label(self.newwin.container, borderwidth=1 , text="Settings - Drone " + str(btn_id))
-        self.newwin.title["font"] = ("Calibri", "9", "bold")
-        self.newwin.title.grid(row=0,column=1)
-
-        self.lblnetwork = Label(self.newwin.container, 
-        text="network:", font=self.fonte, width=15)
-        self.lblnetwork.grid(row=1,column=0)
-
-        self.entnetwork = Entry(self.newwin.container, font=self.fonte, width=15)
-        self.entnetwork.grid(row=1,column=1)
-
-        # self.newwin.container1 = Frame(self.newwin)
-        # self.newwin.container1["padx"] = 20
-        # self.newwin.container1["pady"] = 5
-        # self.newwin.container1.pack()
-
-        # self.lblqgc = Label(self.newwin.container1, 
-        # text="qGroundControl:", font=self.fonte, width=15)
-        # self.lblqgc.pack(side=TOP)
-
-        # self.newwin.exit_btn = Button(self.newwin, text="Exit", 
-        # font=self.fonte, width=15)
-        # self.newwin.exit_btn["command"] = self.newwin.destroy
-        # self.newwin.exit_btn.pack(side=BOTTOM)
-
-        
-    
-    def _add_drone(self, master=None):
+        def _add_drone(self, master=None):
         global id
         id = id + 1
         self.container5 = Frame(master)
@@ -131,8 +89,47 @@ class Application:
         
         print("add_drone opt")
 
+    def _config_base(self):
+        print("config_base")
 
+    def _start_drone(self, btn_id):
+        print("start_drone")
+
+    def _config_drone(self, btn_id, master=None):
+        print("config_drone id: " + str(btn_id))
         
+        # self.newwin = Toplevel(master)
+
+        # self.newwin.container = Frame(self.newwin)
+        # self.newwin.container["pady"] = 10
+        # self.newwin.container.pack()
+
+        # self.newwin.title = Label(self.newwin.container, borderwidth=1 , text="Settings - Drone " + str(btn_id))
+        # self.newwin.title["font"] = ("Calibri", "9", "bold")
+        # self.newwin.title.grid(row=0,column=1)
+
+        # self.lblnetwork = Label(self.newwin.container, 
+        # text="network:", font=self.fonte, width=15)
+        # self.lblnetwork.grid(row=1,column=0)
+
+        # self.entnetwork = Entry(self.newwin.container, font=self.fonte, width=15)
+        # self.entnetwork.grid(row=1,column=1)
+
+        # self.newwin.container1 = Frame(self.newwin)
+        # self.newwin.container1["padx"] = 20
+        # self.newwin.container1["pady"] = 5
+        # self.newwin.container1.pack()
+
+        # self.lblqgc = Label(self.newwin.container1, 
+        # text="qGroundControl:", font=self.fonte, width=15)
+        # self.lblqgc.pack(side=TOP)
+
+        # self.newwin.exit_btn = Button(self.newwin, text="Exit", 
+        # font=self.fonte, width=15)
+        # self.newwin.exit_btn["command"] = self.newwin.destroy
+        # self.newwin.exit_btn.pack(side=BOTTOM)
+                
 root = Tk()
 Application(root)
+root.geometry("300x200+300+300")
 root.mainloop()
